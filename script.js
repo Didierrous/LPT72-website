b2949d75cb9aa971d7d465b51f926d4958d3d46f
Didierrous <148478698+Didierrous@users.noreply.github.com>
2026-04-14 03:55:47 -0600

    otro diseño

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 function abrirModal(img){
-    const modal = document.getElementById("modal");
-    const imgModal = document.getElementById("imgModal");
-
-    imgModal.src = img;
-    modal.style.display = "block";
+    document.getElementById("modal").style.display = "block";
+    document.getElementById("imgModal").src = img;
 }
 
 function cerrarModal(){
     document.getElementById("modal").style.display = "none";
 }
 
-// cerrar dando click afuera
 window.onclick = function(e){
-    const modal = document.getElementById("modal");
-    if(e.target === modal){
-        modal.style.display = "none";
+    if(e.target.id === "modal"){
+        cerrarModal();
     }
 }
