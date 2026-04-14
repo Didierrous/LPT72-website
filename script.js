221dda9f2704357f775a54a827712a1e0f9359fd
Didierrous <148478698+Didierrous@users.noreply.github.com>
2026-04-14 04:00:37 -0600

    otro diseño2

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function abrirModal(img){
-    document.getElementById("modal").style.display = "block";
-    document.getElementById("imgModal").src = img;
-}
-
-function cerrarModal(){
-    document.getElementById("modal").style.display = "none";
-}
-
-window.onclick = function(e){
-    if(e.target.id === "modal"){
-        cerrarModal();
-    }
-}
+console.log("Landing PRO lista 🔥");
